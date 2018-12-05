512ee10f4ca788993e0825bab6c15e08d477e07d
Ricky Lee <rilee@seven.com.au>
2018-12-05 17:51:57 +1100

    Add User App and Models structure

--- src/RecordKeeping/ClientApp/src/app/user/user.component.ts
+++ src/RecordKeeping/ClientApp/src/app/user/user.component.ts
@@ -6,18 +6,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './user.component.html'
 })
 export class UserComponent {
-  public forecasts: WeatherForecast[];
+  public users: User[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
+    http.get<User[]>(baseUrl + 'api/User/Users').subscribe(result => {
+      this.users = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface User {
+  userId: number;
+  username: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  dateOfBirth: Date;
+  email: string;
+  phone: string;
+  mobile: string;
 }
---
+++ src/RecordKeeping/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecordKeeping.Models
+{
+    public class User
+    {
+        [Display(Name = "User ID")]
+        public int UserId { get; set; }
+
+        [Required, StringLength(50)]
+        public string Username { get; set; }
+
+        [Required, StringLength(128), RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")]
+        public string Password { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string Firstname { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Surname")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public Nullable<DateTime> DateOfBirth { get; set; }
+
+        [Required, StringLength(256)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        public string Mobile { get; set; }
+    }
+}
